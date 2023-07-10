8ca6a8c19a13ffa5d145e36cffc053bc4929dcce
Ahmad Khan <77138985+ahmadtkhan@users.noreply.github.com>
2023-07-09 23:52:16 -0400

    Add files via upload

---
+++ time_to_buy_and_sell_stocks/sell_stocks.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int min_val = INT_MAX;
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < min_val){
+                min_val = prices[i];
+            }
+            if(prices[i] <= min_val){
+                min_val = prices[i];
+            }else if(prices[i] - min_val > profit){
+                profit = prices[i] - min_val;
+            }
+        }
+        return profit;
+    }
+};
