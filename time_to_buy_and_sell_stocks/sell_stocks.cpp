708ce330c7849345a9ae781f2f862322adef8d18
Ahmad Khan <77138985+ahmadtkhan@users.noreply.github.com>
2023-07-14 22:50:24 -0400

    Update sell_stocks.cpp

--- time_to_buy_and_sell_stocks/sell_stocks.cpp
+++ time_to_buy_and_sell_stocks/sell_stocks.cpp
@@ -1,18 +1,18 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int profit = 0;
-        int min_val = INT_MAX;
-        for(int i = 0; i < prices.size(); i++){
-            if(prices[i] < min_val){
-                min_val = prices[i];
-            }
-            if(prices[i] <= min_val){
-                min_val = prices[i];
-            }else if(prices[i] - min_val > profit){
-                profit = prices[i] - min_val;
-            }
+#include "sell_stocks.h"
+#include "climits"
+
+int sell_stocks::maxProfit(std::vector<int>& prices) {
+    int profit = 0;
+    int min_val = INT_MAX;
+    for(int i = 0; i < prices.size(); i++){
+        if(prices[i] < min_val){
+            min_val = prices[i];
         }
-        return profit;
+        if(prices[i] <= min_val){
+            min_val = prices[i];
+        }else if(prices[i] - min_val > profit){
+            profit = prices[i] - min_val;
+        }
     }
-};
+    return profit;
+}
